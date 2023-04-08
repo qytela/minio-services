c15e662d2c2bc7b37fd50339a03fdea5e4c4250d
qytela <julfanshahidayah@gmail.com>
2023-04-08 22:20:53 +0700

    refactor(CopyBucket): exec async/await

--- utils/CopyBucket.js
+++ utils/CopyBucket.js
@@ -1,40 +1,33 @@
 const fs = require("fs");
 const chalk = require("chalk");
-const { exec } = require("child_process");
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
 
-const CopyBucket = (source, destination, withRemove) => {
+const CopyBucket = async (source, destination, withRemove) => {
   console.log("Source Bucket =>", chalk.green(source));
   console.log("Destination Bucket =>", chalk.green(destination), "\n");
 
-  exec(`node index db -b ${source}`, (errDownload, outputDownload) => {
-    if (errDownload) {
-      return console.log(errDownload);
-    }
+  try {
+    const { stdout: outputDownload } = await exec(`node index db -b ${source}`);
     if (/Bucket not exists!/g.test(outputDownload)) {
       return console.log(chalk.red(outputDownload));
     }
 
-    exec(
-      `node index ub -s ./downloads/${source} -b ${destination}`,
-      (errUpload, outputUpload) => {
-        if (errUpload) {
-          return console.log(errUpload);
-        }
+    await exec(`node index ub -s ./downloads/${source} -b ${destination}`);
+    console.log(chalk.bgGreen("Success to sync bucket..."));
 
-        console.log(chalk.bgGreen("Success to sync bucket..."));
+    if (withRemove) {
+      console.log(`\n${chalk.bgBlue("Start remove folder...")}`);
 
-        if (withRemove) {
-          console.log(`\n${chalk.bgBlue("Start remove folder...")}`);
+      const folderPath = `./downloads/${source}`;
+      fs.rmSync(folderPath, { recursive: true, force: true });
 
-          const folderPath = `./downloads/${source}`;
-          fs.rmSync(folderPath, { recursive: true, force: true });
-
-          console.log(`Folder '${chalk.green(folderPath)}' removed`);
-          console.log(`${chalk.bgBlue("Finish remove folder...")}`);
-        }
-      },
-    );
-  });
+      console.log(`Folder '${chalk.green(folderPath)}' removed`);
+      console.log(`${chalk.bgBlue("Finish remove folder...")}`);
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = CopyBucket;
